f44d0aa64d098ec26d1dd6cc6ee6f098984eeabe
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-07-06 17:37:10 +0000

    Set up structure of  game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
